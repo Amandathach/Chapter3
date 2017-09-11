eb05431bd10efb395dd7c44a745a222810010bf5
Amandathach <at137892@students.davenport.k12.ia.us>
2017-09-11 09:54:57 -0500

    update work

---
+++ chapter3/src/tutorial/First.java
@@ -0,0 +1,17 @@
+package tutorial;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("What is your address?");
+		Second.displayAddress();
+		
+		
+	
+		
+		public static displayAddress();
+	}
+
+}
---
+++ chapter3/src/tutorial/second.java
@@ -0,0 +1,14 @@
+package tutorial;
+
+public class second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("XYZ Company");
+		System.out.println("8900 U.S. Hwy 14");
+		System.out.println("Crystal Lake, IL 60014");
+		System.out.println("First Java application");
+	}
+
+}
